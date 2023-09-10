517247559a83996c021500ee0cacc52287db7fd5
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-09-10 03:34:18 +0300

    Add files via upload

---
+++ Easy/Data Science Skills [LinkedIn SQL Interview Question].sql
@@ -0,0 +1,21 @@
+WITH skill_count AS
+(
+SELECT candidate_id,
+      SUM(CASE WHEN skill IN ('Python','PostgreSQL','Tableau') THEN 1
+          ELSE 0 END) as skill_sum
+FROM candidates
+GROUP BY candidate_id
+)
+
+SELECT candidate_id 
+FROM skill_count
+WHERE skill_sum = 3
+ORDER BY candidate_id
+
+
+--Anthor Solution
+SELECT candidate_id
+FROM candidates
+WHERE skill = 'Python' OR skill = 'PostgreSQL' OR skill = 'Tableau'
+GROUP BY candidate_id
+HAVING COUNT(skill) >= 3
