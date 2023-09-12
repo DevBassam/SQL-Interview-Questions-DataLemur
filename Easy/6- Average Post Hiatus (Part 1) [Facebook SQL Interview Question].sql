b83eeeaf8b9066fb887c8b2ed3ff948c5e0b0e6f
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-09-12 06:48:06 +0300

    Update 6- Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql

--- Easy/6- Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql
+++ Easy/6- Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql
@@ -1,5 +1,4 @@
-SELECT user_id,
-      DATE_PART('day', MAX(post_date) - MIN(post_date)) AS days_between
+SELECT user_id, DATE_PART('day', MAX(post_date) - MIN(post_date)) AS days_between
 FROM posts
 WHERE post_date > '12/31/2020' AND post_date < '1/1/2022'
 GROUP BY user_id
