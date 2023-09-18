7c6c095bf4a387df713a0e9688731a18d2652be7
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-09-18 06:51:02 +0300

    Add files via upload

---
+++ Medium/2- Sending vs. Opening Snaps [Snapchat SQL Interview Question].sql
@@ -0,0 +1,15 @@
+WITH total_time AS
+(
+SELECT age_bucket,
+        SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END) AS ttl_send,
+        SUM(CASE WHEN activity_type = 'open' THEN time_spent ELSE 0 END) AS ttl_open
+FROM activities
+JOIN age_breakdown
+USING(user_id)
+GROUP BY age_bucket
+)
+
+SELECT age_bucket, 
+       ROUND(ttl_send / (ttl_send+ttl_open) * 100,2) AS send_perc,
+       ROUND(ttl_open / (ttl_send+ttl_open) * 100,2) AS open_perc
+FROM total_time
