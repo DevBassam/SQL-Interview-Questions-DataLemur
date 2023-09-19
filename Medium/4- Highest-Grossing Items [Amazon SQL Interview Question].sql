6996baed4fc31188d5ba6154e2a5b51d8cdec6b6
Bassam Salama <84365449+DevBassam@users.noreply.github.com>
2023-09-19 06:54:04 +0300

    Add files via upload

---
+++ Medium/4- Highest-Grossing Items [Amazon SQL Interview Question].sql
@@ -0,0 +1,8 @@
+SELECT category,	product, total_spend 
+FROM (SELECT category,	product,	SUM(spend) AS total_spend,
+             RANK() OVER(PARTITION BY category ORDER BY SUM(spend) DESC) AS row_num
+      FROM product_spend
+      WHERE transaction_date >= '1/1/2022' AND transaction_date < '1/1/2023'
+      GROUP BY category,	product
+      ORDER BY category, total_spend DESC) AS rank
+WHERE row_num < 3;
